fbbfc2007d250589dcf3aa6ddf55f1a82965b2e0
Ahmed Jadani <ahjadani@e2r10p13.1337.ma>
2022-11-14 16:19:30 +0100

    Hash Based msg aith code - BLAKE2

---
+++ hash.py
@@ -0,0 +1,22 @@
+# BLAKE2 
+from hashlib import blake2b
+from hmac import compare_digest
+
+KEY = b'testkey'
+AUTH_SIZE = 16
+
+def sign(cookie):
+    return blake2b(cookie, key=KEY, digest_size=AUTH_SIZE).hexdigest().encode('utf-8')
+
+def verify(cookie, sig):
+    return compare_digest(sig, sign(cookie))
+
+cookie = b'0xpacman'
+sig = sign(cookie)
+
+print("{0},{1}".format(cookie.decode('utf-8'), sig))
+
+print(verify(b'1xpacman', sig))
+# False
+print(verify(b'0xpacman', sig))
+# True
